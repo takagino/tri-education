73e5e546a49eed1d8f75208f5ddd09f2c5e392f6
takagino <hirotaka.takagi@trident.ac.jp>
2026-09-15 12:39:06 +0900

    0915

--- wf1/2nd/cms/sample-theme/index.php
+++ wf1/2nd/cms/sample-theme/index.php
@@ -34,55 +34,37 @@
     <div class="contents">
       <div class="inner">
         <div class="post-all">
-          <article class="post">
-            <img src="<?php echo esc_url(get_theme_file_uri('/images/pic_post01.jpg')); ?>" alt="">
-            <h2 class="post-title"><?php the_title(); ?></h2>
-            <p class="post-date">
-              <time datetime="<?php echo get_the_date('Y-m-d'); ?>">
-                <?php the_time('y.m.d'); ?>
-              </time>
-            </p>
-            <div class="post-contents">
-              <?php the_excerpt(); ?>
-            </div>
-            <a class="post-btn" href="<?php the_permalink(); ?>">続きを読む</a>
-            <div class="post-info">
-              <ul>
-                <li class="post-category">Category: <?php the_category(' / '); ?></li>
-                <li class="post-tag">Tag: <?php the_tags('', ' / '); ?></li>
-              </ul>
-            </div>
-          </article>
-          <article class="post">
-            <img src="images/pic_post02.jpg" alt="">
-            <h2 class="post-title">記事のタイトル</h2>
-            <p class="post-date">2020年8月12日</p>
-            <div class="post-contents">
-              <p>本文の抜粋を20文字程度表示します。</p>
-            </div>
-            <a class="post-btn" href="single.html">続きを読む</a>
-            <div class="post-info">
-              <ul>
-                <li class="post-category">Category: <a href="#">作品</a></li>
-                <li class="post-tag">Tag: <a href="#">HTML</a> / <a href="#">CSS</a> / <a href="#">JavaScript</a></li>
-              </ul>
-            </div>
-          </article>
-          <article class="post">
-            <img src="images/pic_post03.jpg" alt="">
-            <h2 class="post-title">記事のタイトル</h2>
-            <p class="post-date">2020年8月12日</p>
-            <div class="post-contents">
-              <p>本文の抜粋を20文字程度表示します。</p>
-            </div>
-            <a class="post-btn" href="single.html">続きを読む</a>
-            <div class="post-info">
-              <ul>
-                <li class="post-category">Category: <a href="#">作品</a></li>
-                <li class="post-tag">Tag: <a href="#">HTML</a> / <a href="#">CSS</a> / <a href="#">JavaScript</a></li>
-              </ul>
-            </div>
-          </article>
+
+          <?php
+          if (have_posts()):
+            while (have_posts()):
+              the_post();
+          ?>
+              <article id="post-<?php the_ID(); ?>" <?php post_class('post'); ?>>
+                <img src="<?php echo esc_url(get_theme_file_uri('/images/pic_post01.jpg')); ?>" alt="">
+                <h2 class="post-title"><?php the_title(); ?></h2>
+                <p class="post-date">
+                  <time datetime="<?php echo get_the_date('Y-m-d'); ?>">
+                    <?php the_time('y.m.d'); ?>
+                  </time>
+                </p>
+                <div class="post-contents">
+                  <?php the_excerpt(); ?>
+                </div>
+                <a class="post-btn" href="<?php the_permalink(); ?>">続きを読む</a>
+                <div class="post-info">
+                  <ul>
+                    <li class="post-category">Category: <?php the_category(' / '); ?></li>
+                    <li class="post-tag">Tag: <?php the_tags('', ' / '); ?></li>
+                  </ul>
+                </div>
+              </article>
+
+          <?php
+            endwhile;
+          endif;
+          ?>
+
           <div class="nav-page">
             <ul>
               <li><a href="#">&lt;</a></li>
