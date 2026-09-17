2070ddd37a8b46fb7cf2b166e90bc6e3b2a7d52a
takagino <hirotaka.takagi@trident.ac.jp>
2026-09-17 09:49:47 +0900

    0917

---
+++ wf1/2nd/cms/sample-theme/404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+<main>
+  <div class="contents">
+    <div class="inner">
+      <div class="post-all">
+        <p>ページは存在しません。</p>
+      </div>
+      <div class="sidebar">
+        <?php get_sidebar(); ?>
+      </div>
+    </div>
+  </div>
+</main>
+<?php get_footer(); ?>
