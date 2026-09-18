50c2928c005a37254097322d51770668e4556ed9
takagino <hirotaka.takagi@trident.ac.jp>
2026-09-18 12:44:50 +0900

    0918

--- wf1/2nd/cms/sample-theme/searchform.php
+++ wf1/2nd/cms/sample-theme/searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" class="search-form" action="http://blog.local/">
+<form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
     <label>
         <span class="screen-reader-text">検索:</span>
         <input type="search" class="search-field" placeholder="キーワードを入力…" value="<?php echo get_search_query(); ?>" name="s">
