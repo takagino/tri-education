226d96766a54a8dc01ad5caa8ded424c8094bebd
takagino <hirotaka.takagi@trident.ac.jp>
2026-09-24 13:53:36 +0900

    0924

---
+++ wf1/2nd/cms/sample-theme/template-contact.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: お問い合わせ専用テンプレート
+*/
+?>
+
+<?php get_header(); ?>
+<main class="contact">
+  <div class="inner">
+    <p>プラグインを使って実装します。</p>
+  </div>
+</main>
+<?php get_footer(); ?>
